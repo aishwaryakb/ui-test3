3e6972951b04b48317e1070afd03d8e38621e833
= <aishwaryak183@gmail.com>
2022-11-02 10:40:17 +0530

    updated

--- src/test/java/com/bren/qa/testcases/LoginPageTest.java
+++ src/test/java/com/bren/qa/testcases/LoginPageTest.java
@@ -2,6 +2,7 @@ package com.bren.qa.testcases;
 import java.net.MalformedURLException;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
--- src/test/java/com/bren/qa/testcases/OtpVerificationPageTest.java
+++ src/test/java/com/bren/qa/testcases/OtpVerificationPageTest.java
@@ -3,6 +3,7 @@ import java.net.MalformedURLException;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.Status;
