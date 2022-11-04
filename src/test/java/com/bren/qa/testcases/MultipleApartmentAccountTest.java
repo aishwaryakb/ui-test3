a8ccaada0b4b9213d25f460f0785d6685ffcc12a
Aishwarya k <96522200+aishwaryakb@users.noreply.github.com>
2022-11-04 18:57:32 +0530

    Update MultipleApartmentAccountTest.java

--- src/test/java/com/bren/qa/testcases/MultipleApartmentAccountTest.java
+++ src/test/java/com/bren/qa/testcases/MultipleApartmentAccountTest.java
@@ -139,6 +139,8 @@ public class MultipleApartmentAccountTest extends Base {
 		driver.findElementByXPath("//*[@text = 'Download image file']").click();
 // 		System.out.print(driver.findElementByXPath("//android.widget.Button[@index = '0']").getAttribute("text"));
 		driver.findElementByXPath("//*[@text = 'ALLOW']").click();
+		Thread.sleep(5000);
+		System.out.println(driver.getPageSource());
 		driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
 		String  actualtoastMessage = driver.findElementByXPath("//android.widget.Toast[1]").getAttribute("name");
 		System.out.print(actualtoastMessage);
