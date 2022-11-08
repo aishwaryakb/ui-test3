ea2044118132df21ea4ae177c50466a7b31cd0f9
Aishwarya k <96522200+aishwaryakb@users.noreply.github.com>
2022-11-08 11:33:12 +0530

    Update Base.java

--- src/test/java/com/bren/qa/base/Base.java
+++ src/test/java/com/bren/qa/base/Base.java
@@ -59,7 +59,7 @@ public class Base {
 	public static void initialization() throws MalformedURLException {
 		
 		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 4 API 29");
+// 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 4 API 29");
 		File appdir = new File("src");
 		File app = new File(appdir, "bren-test.apk");
 		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
