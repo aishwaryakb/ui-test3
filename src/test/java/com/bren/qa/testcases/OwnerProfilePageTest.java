2c1e53ac851479adf26792101cebc44d2540262e
Aishwarya k <96522200+aishwaryakb@users.noreply.github.com>
2022-11-17 12:55:50 +0530

    Update OwnerProfilePageTest.java

--- src/test/java/com/bren/qa/testcases/OwnerProfilePageTest.java
+++ src/test/java/com/bren/qa/testcases/OwnerProfilePageTest.java
@@ -13,6 +13,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import com.bren.qa.helper.TapHelper;
 
 import com.aventstack.extentreports.Status;
 import com.bren.qa.base.Base;
