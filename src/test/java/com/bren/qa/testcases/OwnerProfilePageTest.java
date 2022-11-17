779804f35ba697b32ce18532663a03a4ba89e0ab
Aishwarya k <96522200+aishwaryakb@users.noreply.github.com>
2022-11-17 12:33:22 +0530

    Update OwnerProfilePageTest.java

--- src/test/java/com/bren/qa/testcases/OwnerProfilePageTest.java
+++ src/test/java/com/bren/qa/testcases/OwnerProfilePageTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.InvalidElementStateException;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
