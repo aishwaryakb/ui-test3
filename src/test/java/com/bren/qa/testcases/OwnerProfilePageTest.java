e2895da420660e97379ce3c3d62d870f1295bf53
Aishwarya k <96522200+aishwaryakb@users.noreply.github.com>
2022-11-18 11:03:42 +0530

    Update OwnerProfilePageTest.java

--- src/test/java/com/bren/qa/testcases/OwnerProfilePageTest.java
+++ src/test/java/com/bren/qa/testcases/OwnerProfilePageTest.java
@@ -47,7 +47,7 @@ public class OwnerProfilePageTest extends Base {
 		singleApartmentHomePage = new SingleApartmentHomePage();
 		
 	}
-	@Test(priority = 1)
+// 	@Test(priority = 1)
 	public void verifyDetailsInProfilePage() throws InterruptedException {
 		Thread.sleep(5000);
 		Assert.assertTrue(ownerProfilePage.profilePicture.isDisplayed(), "Profile Picture isn't displayed");
@@ -73,7 +73,7 @@ public class OwnerProfilePageTest extends Base {
 		ExtentManager.getExtentTest().log(Status.PASS, "LogOut Button is displayed");
 	}
 	
-	@Test(priority = 2)
+// 	@Test(priority = 2)
 	public void verifyThatTheNameEmailAddressAndPhoneNumberFieldsAreNonEditable() throws InterruptedException {
 		Thread.sleep(5000);
 		String preSetFieldValue;
@@ -114,7 +114,7 @@ public class OwnerProfilePageTest extends Base {
 		ExtentManager.getExtentTest().log(Status.PASS, "Mobile Input field non editable");
 		
 	}
-	@Test(priority = 3)
+// 	@Test(priority = 3)
 	public void verifyThatTheUserCanLogOutTheAccountByClickingOnTheLogoutButton() {
 		
 		ownerProfilePage.logOutButton.click();
@@ -123,7 +123,7 @@ public class OwnerProfilePageTest extends Base {
 		ExtentManager.getExtentTest().log(Status.PASS, "Logout was successful");
 		
 	}
-	@Test(priority = 5)
+// 	@Test(priority = 5)
 	public void verifyThatTheUserIsNavigatingToTheHomeScreenWhenClickingOnTheBrensIconFromOwnerProfilePage() throws InterruptedException {
 		driver.findElementByXPath("//*[@resource-id ='RNE__Image']").click();
 		myHomePage.homePageVerification();
@@ -131,7 +131,7 @@ public class OwnerProfilePageTest extends Base {
 				+ " Screen When clicking on the Brens Icon from Owner Profile Page");
 	}
 	
-// 	@Test(priority = 5)
+	@Test(priority = 5)
 	public void verifyThatUserCanUpdateProfilePictureFromTheProfileScreen() throws InterruptedException {
 		String expectedToastMessage = "Customer's Profile Photo updated successfully";
 		Thread.sleep(4000);
