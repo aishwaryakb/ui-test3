74f4edd80ae080d4097a1270765d34b39d5226bf
= <aishwaryak183@gmail.com>
2022-11-24 10:56:50 +0530

    reolve socket-hang

--- src/test/java/com/bren/qa/base/Base.java
+++ src/test/java/com/bren/qa/base/Base.java
@@ -25,48 +25,48 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class Base {
-	public static AndroidDriver<AndroidElement> driver;
-	public static Properties prop;
-	public static SoftAssert softAssert;
-	@BeforeSuite
-	public void setupSuite() {
-		ExtentReport.initReport();
-	}
-	@AfterSuite
-	public void tearDownSuite() {
-		ExtentReport.teardownReports();	
-	}
-	@AfterMethod(alwaysRun=true)
-   	 public void tearDown()throws InterruptedException {
-	    driver.close();
+    public static AndroidDriver<AndroidElement> driver;
+    public static Properties prop;
+    public static SoftAssert softAssert;
+    @BeforeSuite
+    public void setupSuite() {
+        ExtentReport.initReport();
+    }
+    @AfterSuite
+    public void tearDownSuite() {
+        ExtentReport.teardownReports(); 
+    }
+    @AfterMethod(alwaysRun=true)
+     public void tearDown()throws InterruptedException {
+        driver.close();
             driver.quit();
-	    Thread.sleep(8000);
-    	}
-	public Base() {
-		try {
-			prop = new Properties();
-			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+
-									"/src/test/java/com/bren/qa/properties/config.properties");
-			prop.load(ip);
-		}
-		catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public static void initialization() throws MalformedURLException {
-		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6");
-		File appdir = new File("src");
-		File app = new File(appdir, "bren-test.apk");
-		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-		cap.setCapability("appPackage","com.brencorp.play.mybren");
-		cap.setCapability("appActivity","com.brencorp.play.mybren.MainActivity");
-		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
-		cap.setCapability("newCommandTimeout", 1000);
-		driver = new AndroidDriver<AndroidElement>(new URL(prop.getProperty("url")),cap);
-		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
-	}
+        }
+    public Base() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+
+                                    "/src/test/java/com/bren/qa/properties/config.properties");
+            prop.load(ip);
+        }
+        catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void initialization() throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6");
+        File appdir = new File("src");
+        File app = new File(appdir, "bren-test.apk");
+        cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        cap.setCapability("appPackage","com.brencorp.play.mybren");
+        cap.setCapability("appActivity","com.brencorp.play.mybren.MainActivity");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+        cap.setCapability("newCommandTimeout", 1000);
+        cap.setCapability("systemPort", UrlUtil.getPort());
+        driver = new AndroidDriver<AndroidElement>(new URL(prop.getProperty("url")),cap);
+        driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
 }
--- src/test/java/com/bren/qa/utils/UrlUtil.java
+++ src/test/java/com/bren/qa/utils/UrlUtil.java
@@ -3,11 +3,9 @@ package com.bren.qa.utils;
 import java.util.Random;
 
 public class UrlUtil {
-    public static String getUrl() {
+    public static int getPort() {
         Random r = new Random();
-        String[] ports={"4723", "4724", "4725", "4726", "4727"}; 
-        String baseUrl = "http://127.0.0.1:4723/wd/hub";
-        String url = baseUrl.substring(0, baseUrl.lastIndexOf(":"))+":"+ports[r.nextInt(ports.length)]+"/wd/hub";
-        return url;
+        int[] ports={ 8200, 8201 }; 
+        return ports[r.nextInt(ports.length)];
     }
 }
