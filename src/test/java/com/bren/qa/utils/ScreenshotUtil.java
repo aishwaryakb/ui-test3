ec47da7446fa5be67edf145e3e823d1e051482ed
Aishwarya k <96522200+aishwaryakb@users.noreply.github.com>
2022-11-29 16:56:03 +0530

    Update ScreenshotUtil.java

--- src/test/java/com/bren/qa/utils/ScreenshotUtil.java
+++ src/test/java/com/bren/qa/utils/ScreenshotUtil.java
@@ -5,7 +5,7 @@ import com.bren.qa.base.Base;
 import java.io.File;
 
 public final class ScreenshotUtil {
-	public static FILE getBase64Image() {
+	public static File getBase64Image() {
 		return ((TakesScreenshot)Base.driver).getScreenshotAs(OutputType.FILE);
 	}
 }
