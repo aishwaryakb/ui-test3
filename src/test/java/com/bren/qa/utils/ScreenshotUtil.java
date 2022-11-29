f0532a1349263e9d0710c414a3ff5394a38ffb4e
Aishwarya k <96522200+aishwaryakb@users.noreply.github.com>
2022-11-29 16:37:50 +0530

    Update ScreenshotUtil.java

--- src/test/java/com/bren/qa/utils/ScreenshotUtil.java
+++ src/test/java/com/bren/qa/utils/ScreenshotUtil.java
@@ -2,6 +2,7 @@ package com.bren.qa.utils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import com.bren.qa.base.Base;
+import java.io.File;
 
 public final class ScreenshotUtil {
 	public static FILE getBase64Image() {
